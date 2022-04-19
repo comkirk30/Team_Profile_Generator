3bf48706c84b3856521bc0980279a5fff1fdc198
Courtney <courtneymkirk@hotmail.com>
2022-04-18 20:29:56 -0400

    added intern info for index.js

--- index.js
+++ index.js
@@ -130,8 +130,8 @@ const askEngineer = () => {
     ])
 
     .then((engAnswers) => {
-        const manager = new Manager(engAnswers.name, engAnswers.id, engAnswers.email, engAnswers.officeNumber)
-        team.push(manager)
+        const engineer = new Engineer(engAnswers.name, engAnswers.id, engAnswers.email, engAnswers.github)
+        team.push(engineer)
         switch(engAnswers.addTeamMember) {
             case 'Engineer':
                 askEngineer();
@@ -144,10 +144,78 @@ const askEngineer = () => {
         }
     })
     
+    const askIntern = () => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the intern name?',
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Enter the intern name"
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is intern ID?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is intern email?',
+                validate: (value) => {
+                    if (emailValidator.validate(value)) {
+                        return true
+                    } else {
+                       return 'Enter a valid intern email'
+                    }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the intern school?',
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Enter the intern school"
+             }
+            },
+            {
+                type: 'list',
+                name: 'addTeamMember',
+                message: 'What kind of new team member are you adding?',
+                choices: ['Engineer', 'Intern', 'Decline to add new member'],
+            }
+     ])
 
+        .then((intAnswers) => {
+            const intern = new Intern(intAnswers.name, intAnswers.id, intAnswers.email, intAnswers.school)
+            team.push(intern)
+            switch(intAnswers.addTeamMember) {
+                case 'Engineer':
+                    askEngineer();
+                    break;
+                case 'Intern':
+                    askIntern();
+                    break;
+                default:
+                    writeToFile('dist\index.html', createTeam(team))
+            }
+    })
+  }
 
+  askManager();
 
-
-
-
-    
+  function writeToFile(filename, data) {
+      fs.writeFile(filename, data, (err) => {
+          if(err) throw err;
+          console.log('file successfully saved')
+      });
+      
+  };
+  
